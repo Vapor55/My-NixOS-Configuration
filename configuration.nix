00417b21e4bb26c0379dcc7e3906b97fdccdd336
Guilherme Costa de Menezes <guilhermecosta6777@gmail.com>
2025-10-31 10:21:28 -0300

    Added Steam to configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -118,6 +118,17 @@
     ranger
   ]; 
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "steam"
+    "steam-unwrapped"
+  ];
+
   fonts.packages = with pkgs; [
     noto-fonts
     noto-fonts-cjk-sans
