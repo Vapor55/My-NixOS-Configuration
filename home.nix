b4a02bf184fa0c7871291550d83a76484b15c3bc
Guilherme Costa de Menezes <guilhermecosta6777@gmail.com>
2025-11-27 10:54:39 -0300

    Fixed Hyprland Themes error and updated update command alias

--- home.nix
+++ home.nix
@@ -27,7 +27,7 @@
       lla = "ls -la";
       lal = "ls -al";
       la = "ls -a";
-      update = "nh os switch";
+      update = "sudo nh os switch";
       update-home = "nh home switch";
     };
 
@@ -49,6 +49,33 @@
           tags = [ "as:theme" "depth:1" ];
         }
       ];
+    };
+  };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
+  };
+
+  gtk = {
+    enable = true;
+
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Grey-Darkest";
+    };
+
+    iconTheme = {
+      package = pkgs.adwaita-icon-theme;
+      name = "Adwaita";
+    };
+
+    font = {
+      name = "Sans";
+      size = 11;
     };
   };
 
