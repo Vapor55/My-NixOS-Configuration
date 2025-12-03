a27be8c9650519d445b82fd3ebbdbad4f6e59a49
Guilherme Costa de Menezes <guilhermecosta6777@gmail.com>
2025-12-03 19:31:37 -0300

    Added Kio

--- configuration.nix
+++ configuration.nix
@@ -128,6 +128,7 @@
     # Dolphin File Manager
 
     kdePackages.dolphin # This is the actual dolphin package
+    kdePackages.kio # needed since 25.11
     kdePackages.kio-fuse #to mount remote filesystems via FUSE
     kdePackages.kio-extras #extra protocols support (sftp, fish and more)
     kdePackages.qtsvg
