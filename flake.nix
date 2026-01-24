c64eff755bebd3d299427ddc0cccd0eb52bd59c2
Guilherme Costa de Menezes <guilhermecosta6777@gmail.com>
2026-01-24 10:31:58 -0300

    Added CachyOS kernel

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
+    nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -11,13 +13,35 @@
 
   };
 
-  outputs = {self, nixpkgs, home-manager, } @ inputs: {
+  outputs = {self, nixpkgs, nix-cachyos-kernel, home-manager, } @ inputs: {
     
     nixosConfigurations = {
       "Gui-Negativo-2" = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs;};
         system = "x86_64-linux";
-        modules = [./configuration.nix ];
+        modules = [
+
+          (
+            { pkgs, ... }:
+            {
+              nixpkgs.overlays = [
+                # Build the kernels on top of nixpkgs version in your flake.
+                # Binary cache may be unavailable for the kernel/nixpkgs version combos.
+                nix-cachyos-kernel.overlays.default
+
+                # Alternatively: use the exact kernel versions as defined in this repo.
+                # Guarantees you have binary cache.
+                nix-cachyos-kernel.overlays.pinned
+
+                # Only use one of the two overlays!
+              ];
+
+              # ... your other configs 
+            }
+          )
+
+          ./configuration.nix
+        ];
       };
     };
 
