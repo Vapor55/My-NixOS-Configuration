5a6bf0e46e7c051527e50f4f7d869c8a2dcacb48
Guilherme Costa de Menezes <guilhermecosta6777@gmail.com>
2026-01-25 15:52:01 -0300

    Removed CachyOS kernel

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
 
   };
 
-  outputs = {self, nixpkgs, home-manager, } @ inputs: {
+  outputs = {self, nixpkgs,  home-manager, } @ inputs: {
     
     nixosConfigurations = {
       "Gui-Negativo-2" = nixpkgs.lib.nixosSystem {
