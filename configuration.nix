d6199a42b4669adad5afffab6d057cffd6118f5c
Guilherme Costa de Menezes <guilhermecosta6777@gmail.com>
2026-03-01 22:41:18 -0300

    lock WO Mic in firewall

--- configuration.nix
+++ configuration.nix
@@ -217,7 +217,7 @@
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
-  networking.firewall.allowedUDPPorts = [ 49152 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
 
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
