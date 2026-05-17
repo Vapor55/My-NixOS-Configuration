8319021f83a0afcb3036b669df445316498a3e11
Guilherme Costa de Menezes <guilhermecosta6777@gmail.com>
2026-05-17 14:04:19 -0300

    Removed LightDM

--- desktop-lxqt.nix
+++ desktop-lxqt.nix
@@ -10,19 +10,5 @@
   # Exclude default packages from LXQt:
   environment.lxqt.excludePackages = with pkgs; [
     lxqt.qterminal
-  ];
-  
-  # Optional: Configure display manager (e.g., sddm is popular with LXQt)
-  services.xserver.displayManager.lightdm = {
-    enable = true;
-
-    # Setting gtk as the greeter
-    greeters.gtk.enable = true;
-
-    # Example of having background as a particular color
-    # background = "#ffa07a";
-
-    # Example of the default image background (must be an absolute path)
-    background = ./Assets/Wallpapers/Saki-Tenma/wp11393211-saki-tenma-wallpapers.png;
-  };
+  ]; 
 }
